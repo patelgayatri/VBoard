29839d5a7e1385505c9574bc180d69cfd7c22bd5
patelgayatri <gayatripatel566@gmail.com>
2022-02-07 15:05:53 +1100

    -api integration

--- app/src/main/java/com/techand/vboard/data/repository/VideosRepository.kt
+++ app/src/main/java/com/techand/vboard/data/repository/VideosRepository.kt
@@ -11,17 +11,6 @@ import javax.inject.Inject
 
 class VideosRepository @Inject constructor(private val service: VideosApi) {
 
-//    suspend fun getVideos(page: Long): Flow<Result<List<Aweme>>> =
-//        service.fetchVideos(page)
-//            .map {
-//                if (it.isSuccess)
-//                    Result.success(it.getOrNull()!!)
-//                else
-//                    Result.failure(it.exceptionOrNull()!!)
-//            }
-
-
-
     fun fetchPosts(): Flow<PagingData<Content>> {
         return Pager(
             PagingConfig(pageSize = 10, enablePlaceholders = false)
--- app/src/main/java/com/techand/vboard/network/VideosApi.kt
+++ app/src/main/java/com/techand/vboard/network/VideosApi.kt
@@ -8,8 +8,8 @@ import retrofit2.http.Query
 interface VideosApi {
 
     @Headers(
-        "x-rapidapi-key:8e9703ee34mshab2b6c42aaef862p11e120jsnd7fd67f3bcec",
-        "x-rapidapi-host:youtube-search-and-download.p.rapidapi.com"
+        apikey,
+        apiHost
     )
 //    @GET("search/top?query=top&sort=1")
 //    suspend fun fetchVideos(@Query("offset") query: Int): VideoResult
